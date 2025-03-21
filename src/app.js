232cf8c65b039535ea43f21f7e9e4457ca4ed66e
RohitBCA456 <ry2281104@gmail.com>
2025-03-21 20:51:55 +0530

    feat: enhance CORS configuration to specify allowed methods

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ import cookieParser from "cookie-parser";
 const app = express();
 app.use(
   cors({
-    origin: "https://blooddonationdib.netlify.app",
-    credentials: true,
+    origin: "https://blooddonationdib.netlify.app", // Your frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allow the HTTP methods you use
+    credentials: true, // Allow cookies and credentials
   })
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
